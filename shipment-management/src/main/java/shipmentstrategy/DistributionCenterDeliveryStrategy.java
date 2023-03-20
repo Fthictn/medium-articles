02d3857fa7a8ba558df5a1e5c7ff5a8d5bf13e39
fthi.cetin@gmail.com <fthi.cetin@gmail.com>
2023-03-20 21:12:47 +0300

    some business logic added

--- shipment-management/src/main/java/shipmentstrategy/DistributionCenterDeliveryStrategy.java
+++ shipment-management/src/main/java/shipmentstrategy/DistributionCenterDeliveryStrategy.java
@@ -1,6 +1,5 @@
 package shipmentstrategy;
 
-import model.constant.ShipmentType;
 import model.constant.State;
 import model.constant.contract.ShipmentDeliveryRequest;
 import model.constant.contract.ShipmentDeliveryResponse;
@@ -12,17 +11,18 @@ import java.util.function.Predicate;
 
 public class DistributionCenterDeliveryStrategy implements ShipmentDelivery {
 
-    private final long WEIGHT_LIMIT = 100;
+    private final long MIN_WEIGHT_LIMIT = 100;
+    private final long MAX_WEIGHT_LIMIT = 5000;
     @Override
     public ShipmentDeliveryResponse deliver(ShipmentDeliveryRequest request) {
         ShipmentDeliveryResponse response = new ShipmentDeliveryResponse();
         List<Shipment> shipments = request.getShipments();
-        Predicate<Shipment> isPackage = shipment -> ShipmentType.PACKAGE.equals(shipment.getType());
-        Predicate<Shipment> isUnderWeightLimit = shipment -> shipment.getWeight() <= WEIGHT_LIMIT;
+        Predicate<Shipment> isUnderWeightLimit = shipment -> shipment.getWeight() <= MAX_WEIGHT_LIMIT;
+        Predicate<Shipment> isOverOfWeightLimit = shipment -> shipment.getWeight() <= MIN_WEIGHT_LIMIT;
         Predicate<Shipment> isInCreatedState = shipment -> shipment.getState() == State.CREATED;
         shipments.stream()
                 .filter(isInCreatedState)
-                .filter(isPackage)
+                .filter(isOverOfWeightLimit)
                 .filter(isUnderWeightLimit)
                 .forEach(shipment -> shipment.setState(State.LOADED));
 
